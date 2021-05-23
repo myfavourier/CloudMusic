917e7ccfd0e44fb9f661e973bcef3e4642f47f9c
myfavourier <781007782@qq.com>
2021-05-23 19:07:11 +0800

    bottom_view done

---
+++ cloud_music/src/main/java/com/example/cloud_music/application/ImoocVoiceApplication.kt
@@ -0,0 +1,32 @@
+package com.example.cloud_music.application
+
+import android.app.Application
+import com.example.lib_audio.app.AudioHelper
+
+
+class ImoocVoiceApplication : Application() {
+
+    //伴身对象实现单例
+    companion object {
+        lateinit var mInstance: ImoocVoiceApplication
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        mInstance = this
+        init()
+    }
+
+    private fun init() {
+        //视频SDK初始化
+//        VideoHelper.init(mInstance)
+        //音频SDK初始化
+        AudioHelper.init(mInstance)
+        //分享SDK初始化
+//        ShareManager.initSDK(mInstance)
+//        //更新组件下载
+//        UpdateHelper.init(mInstance)
+//        //ARouter初始化
+//        ARouter.init(mInstance)
+    }
+}
